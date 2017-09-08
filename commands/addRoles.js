5944f3836a744420da99e745fafa75638d03a11e
Logiz <l.samemami@gmail.com>
2017-09-08 08:49:11 +0200

    Added more roles & rewrite

--- commands/addRoles.js
+++ commands/addRoles.js
@@ -6,101 +6,59 @@ const utils = require("../src/utils");
 let addRoles = {
   pattern: /!ios|!android|!linux/i,
   execute: function(bot, channelID, userTag, userID, command, msg) {
-    let roles = msg.member.roles;
+    let allRoles = msg.member.roles;
+    let cn = command.toLowerCase();
+    let roleID;
+    let roleName;
+
     switch (command.toLowerCase()) {
-      case "!android":
-        var android = config.roles.androidAlphaRole;
-        var index = roles.indexOf(android);
-        if(index === -1){
-          roles.push(android);
-          bot.editGuildMember(msg.channel.guild.id, userID, {
-            roles: roles
-          }).then(() => {
-            bot.createMessage(channelID, "<@" + userID + ">, you have been given the role of `Android Alpha`. Use the same command again to remove this role from yourself.").then(utils.delay(customConfig.delayInS)).then((msgInfo) => {
-              bot.deleteMessage(channelID, msgInfo.id).catch(() => {});
-              bot.deleteMessage(channelID, msg.id).catch(() => {});
-            }).catch((err) => {
-              console.log("--> AddRoles | Android addRole\n" + err);
-            });
-            bot.createMessage(config.channels.modLogChannel, ":robot: Gave `Android Alpha` to **" + userTag + "**");
-          });
-        }else{
-          roles.splice(index, 1);
-          bot.editGuildMember(msg.channel.guild.id, userID, {
-            roles: roles
-          }).then(() => {
-            bot.createMessage(channelID, "<@" + userID + ">, you have been removed from the `Android Alpha` role. Use the same command again to add this role to yourself.").then(utils.delay(customConfig.delayInS)).then((msgInfo) => {
-              bot.deleteMessage(channelID, msgInfo.id).catch(() => {});
-              bot.deleteMessage(channelID, msg.id).catch(() => {});
-            }).catch((err) => {
-              console.log("--> AddRoles | Android removeRole\n" + err);
-            });
-            bot.createMessage(config.channels.modLogChannel, ":robot: Removed `Android Alpha` from **" + userTag + "**");
-          });
-        }
-      break;
-      case "!ios":
-        var ios = config.roles.iosTestflightRole;
-        var index = roles.indexOf(ios);
-        if(index === -1){
-          roles.push(ios);
-          bot.editGuildMember(msg.channel.guild.id, userID, {
-            roles: roles
-          }).then(() => {
-            bot.createMessage(channelID, "<@" + userID + ">, you have been given the role of `iOSTestflight`. Use the same command again to remove this role from yourself.").then(utils.delay(customConfig.delayInS)).then((msgInfo) => {
-              bot.deleteMessage(channelID, msgInfo.id).catch(() => {});
-              bot.deleteMessage(channelID, msg.id).catch(() => {});
-            }).catch((err) => {
-              console.log("--> AddRoles | iOS addRole\n" + err);
-            });
-            bot.createMessage(config.channels.modLogChannel, ":iphone: Gave `iOSTestflight` to **" + userTag + "**");
-          });
-        }else{
-          roles.splice(index, 1);
-          bot.editGuildMember(msg.channel.guild.id, userID, {
-            roles: roles
-          }).then(() => {
-            bot.createMessage(channelID, "<@" + userID + ">, you have been removed from the `iOSTestflight` role. Use the same command again to add this role to yourself.").then(utils.delay(customConfig.delayInS)).then((msgInfo) => {
-              bot.deleteMessage(channelID, msgInfo.id).catch(() => {});
-              bot.deleteMessage(channelID, msg.id).catch(() => {});
-            }).catch((err) => {
-              console.log("--> AddRoles | iOS removeRole\n" + err);
-            });
-            bot.createMessage(config.channels.modLogChannel, ":iphone: Removed `iOSTestflight` from **" + userTag + "**");
-          });
-        }
-      break;
-      case "!linux":
-        var linux = config.roles.linuxTesterRole;
-        var index = roles.indexOf(linux);
-        if(index === -1){
-          roles.push(linux);
-          bot.editGuildMember(msg.channel.guild.id, userID, {
-            roles: roles
-          }).then(() => {
-            bot.createMessage(channelID, "<@" + userID + ">, you have been given the role of `Linux Tester`. Use the same command again to remove this role from yourself.").then(utils.delay(customConfig.delayInS)).then((msgInfo) => {
-              bot.deleteMessage(channelID, msgInfo.id).catch(() => {});
-              bot.deleteMessage(channelID, msg.id).catch(() => {});
-            }).catch((err) => {
-              console.log("--> AddRoles | Linux Tester addRole\n" + err);
-            });
-            bot.createMessage(config.channels.modLogChannel, ":penguin: Gave `Linux Tester` to **" + userTag + "**");
-          });
-        }else{
-          roles.splice(index, 1);
-          bot.editGuildMember(msg.channel.guild.id, userID, {
-            roles: roles
-          }).then(() => {
-            bot.createMessage(channelID, "<@" + userID + ">, you have been removed from the `Linux Tester` role. Use the same command again to add this role to yourself.").then(utils.delay(customConfig.delayInS)).then((msgInfo) => {
-              bot.deleteMessage(channelID, msgInfo.id).catch(() => {});
-              bot.deleteMessage(channelID, msg.id).catch(() => {});
-            }).catch((err) => {
-              console.log("--> AddRoles | Linux Tester removeRole\n" + err);
-            });
-            bot.createMessage(config.channels.modLogChannel, ":penguin: Removed `Linux Tester` from **" + userTag + "**");
-          });
-        }
-      break;
+      case '!android':
+        roleID = config.roles.androidAlphaRole;
+        roleName = ':robot: `Android Alpha`';
+        break;
+      case '!ios':
+        roleID = config.roles.iosTestflightRole;
+        roleName = ':iphone: `iOS Test Flight`'
+        break;
+      case '!linux':
+        roleID = config.roles.linuxTesterRole;
+        roleName = ':penguin: `Linux Tester`';
+        break;
+      case '!windows':
+        roleID = config.roles.windowsRole;
+        roleName = '`Windows`';
+        break;
+      case '!mac':
+        roleID = config.roles.macRole;
+        roleName = '`Mac`';
+        break;
+      case '!canary':
+        roleID = config.roles.canaryRole;
+        roleName = '`Canary`';
+        break;
+    }
+
+    let index = allRoles.indexOf(roleID);
+    if (index === -1) {
+      allRoles.push(roleID);
+      bot.editGuildMember(msg.channel.guild.id, userID, {
+        roles: allRoles
+      }).then(() => {
+        utils.botReply(bot, userID, channelID, `You gave yourself ${roleName}. Use the same command again to remove the role from yourself.`, command, msg.id);
+        bot.createMessage(config.channels.modLogChannel, `${roleName} added to ${userTag}`);
+      }).catch((err) => {
+        console.log('--> addRole | uID: ' + userID + " rID: " + roleID + "\n" + err);
+      });
+    } else {
+      allRoles.splice(index, 1);
+      bot.editGuildMember(msg.channel.guild.id, userID, {
+        roles: allRoles
+      }).then(() => {
+        utils.botReply(bot, userID, channelID, `You removed ${roleName} from yourself. Use the same command again to add the role from yourself.`, command, msg.id);
+        bot.createMessage(config.channels.modLogChannel, `${roleName} removed from ${userTag}`);
+      }).catch((err) => {
+        console.log('--> removeRole | uID: ' + userID + " rID: " + roleID + "\n" + err);
+      });
     }
   },
   roles: [
